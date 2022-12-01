8f8a6ddfad8016449a2186680535d0f3df5c2cb8
xWafl <73samepleperson@gmail.com>
2022-12-01 06:16:40 -0500

    feat: day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,26 +1,36 @@
 import * as path from "path";
 import readInput, { InputMode } from "../helpers/readInput";
 
-const part1 = (input: string) => {
-
+const part1 = (input: number[][]) => {
+    return Math.max(...input.map(l => l.reduce((acc, cur) => acc + cur, 0)));
 };
 
-const part2 = (input: string) => {
-
+const part2 = (input: number[][]) => {
+    const sorted = input.map(l => l.reduce((acc, cur) => acc + cur, 0)).sort((a, b) => b - a);
+    return sorted[0] + sorted[1] + sorted[2];
 };
 
 const main = async () => {
-    const input = await readInput(path.join(__dirname, "./input.txt"), InputMode.Raw);
+    const input = await readInput(path.join(__dirname, "./input.txt"), InputMode.Split);
+    const parsed: number[][] = [[]];
+
+    for (const line of input) {
+        if (line === "") {
+            parsed.push([]);
+        } else {
+            parsed[parsed.length - 1].push(Number(line));
+        }
+    }
 
     console.time("part1");
 
-    console.log(part1(input));
+    console.log(part1(parsed));
 
     console.timeEnd("part1");
 
     console.time("part2");
 
-    console.log(part2(input));
+    console.log(part2(parsed));
 
     console.timeEnd("part2");
 };

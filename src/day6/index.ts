7d44f0e7da52a29967d2c545ae222386711c53b6
xWafl <73samepleperson@gmail.com>
2022-12-06 06:33:35 -0500

    feat: day 6

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -2,11 +2,17 @@ import * as path from "path";
 import readInput, { InputMode } from "../helpers/readInput";
 
 const part1 = (input: string) => {
-
+    for (let i = 0; i < input.length - 4; i++) {
+        const hasDuplicate = [...new Set([...input.slice(i, i + 4)])].length !== 4;
+        if (!hasDuplicate) return i + 4;
+    }
 };
 
 const part2 = (input: string) => {
-
+    for (let i = 0; i < input.length - 14; i++) {
+        const hasDuplicate = [...new Set([...input.slice(i, i + 14)])].length !== 14;
+        if (!hasDuplicate) return i + 14;
+    }
 };
 
 const main = async () => {

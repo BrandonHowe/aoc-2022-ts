0261ea01c301d8b62736cebeca72070f46474ad5
xWafl <73samepleperson@gmail.com>
2022-12-20 19:23:55 -0500

    feat: day 20

--- src/day20/index.ts
+++ src/day20/index.ts
@@ -1,16 +1,40 @@
 import * as path from "path";
 import readInput, { InputMode } from "../helpers/readInput";
 
-const part1 = (input: string) => {
+const part1 = (input: number[]) => {
+    const mapped = input.map((l, idx) => [l, idx]);
 
+    for (let i = 0; i < input.length; i++) {
+        const target = mapped.findIndex(l => l[1] === i);
+        const val = mapped[target][0];
+        mapped.splice(target, 1);
+        mapped.splice((target + val) % mapped.length, 0, [val, i]);
+    }
+
+    const newOrder = mapped.map(l => l[0]);
+    const zeroPlace = newOrder.indexOf(0);
+    return newOrder[(zeroPlace + 1000) % newOrder.length] + newOrder[(zeroPlace + 2000) % newOrder.length] + newOrder[(zeroPlace + 3000) % newOrder.length];
 };
 
-const part2 = (input: string) => {
+const part2 = (input: number[]) => {
+    const mapped = input.map(l => l * 811589153).map((l, idx) => [l, idx]);
 
+    for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < input.length; i++) {
+            const target = mapped.findIndex(l => l[1] === i);
+            const val = mapped[target][0];
+            mapped.splice(target, 1);
+            mapped.splice((target + val) % mapped.length, 0, [val, i]);
+        }
+    }
+
+    const newOrder = mapped.map(l => l[0]);
+    const zeroPlace = newOrder.indexOf(0);
+    return newOrder[(zeroPlace + 1000) % newOrder.length] + newOrder[(zeroPlace + 2000) % newOrder.length] + newOrder[(zeroPlace + 3000) % newOrder.length];
 };
 
 const main = async () => {
-    const input = await readInput(path.join(__dirname, "./input.txt"), InputMode.Raw);
+    const input = await readInput(path.join(__dirname, "./input.txt"), InputMode.SplitNum);
 
     console.time("part1");
 
